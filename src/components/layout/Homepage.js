9d502a17a019299429ef4713d4d3521fc3396b9e
Eray Chumak <eray_chumak@protonmail.com>
2020-06-14 14:52:50 +0100

    move these nav links into Nav component

--- src/components/layout/Homepage.js
+++ src/components/layout/Homepage.js
@@ -1,16 +1,8 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
-import { Link } from "react-router-dom";
 
 export const Homepage = () => {
   return (
     <div>
-      <Link to="/single-player">
-        <Button>Detailed Search</Button>
-      </Link>
-      <Link to="/teambuilder">
-        <Button>Build a Team</Button>
-      </Link>
     </div>
   );
 };
