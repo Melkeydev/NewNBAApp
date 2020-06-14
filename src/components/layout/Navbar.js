38973e27da77cbc4416821b7089206794cb7b324
Eray Chumak <eray_chumak@protonmail.com>
2020-06-14 14:52:30 +0100

    fix missing Menu.Item component for every nav link

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,42 +1,80 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { Menu, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import { logout } from "../../actions/AuthAction";
 import { useDispatch, useSelector } from "react-redux";
 
 export const Navbar = () => {
+  const dispatch = useDispatch();
+
+  const [ active, setActive ] = useState("home");
   const { isLoggedIn } = useSelector((state) => state.Auth);
 
-  const dispatch = useDispatch();
+  const handleClick = useCallback((_, { name }) => setActive(name), []);
 
   return (
     <div>
-      <Menu>
-        <Link to="/">
-          <Button>Home</Button>
-        </Link>
+      <Menu stackable>
+        <Menu.Item
+          name="home"
+          onClick={handleClick}
+          active={active === "home"}
+        >
+          <Link to="/">
+            <Button>Home</Button>
+          </Link>
+        </Menu.Item>
+        <Menu.Item
+          name="singlePlayer"
+          onClick={handleClick}
+          active={active === "singlePlayer"}
+        >
+          <Link to="/single-player">
+            <Button>Detailed Search</Button>
+          </Link>
+        </Menu.Item>
+        <Menu.Item
+            name="teamBuilder"
+            onClick={handleClick}
+            active={active === "teamBuilder"}
+          >
+          <Link to="/teambuilder">
+            <Button>Build a Team</Button>
+          </Link>
+        </Menu.Item>
 
         <Menu.Menu position="right">
           {isLoggedIn && (
-            <Link to="/" style={{ textDecoration: "none", color: "black" }}>
-              <Button onClick={() => dispatch(logout())}>Logout</Button>
-            </Link>
+            <Menu.Item
+                name="home"
+                onClick={handleClick}
+              >
+              <Link to="/">
+                <Button onClick={() => dispatch(logout())}>Logout</Button>
+              </Link>
+            </Menu.Item>
           )}
           {!isLoggedIn && (
-            <Link
-              to="/login"
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              <Button>Login</Button>
-            </Link>
+            <Menu.Item
+                name="login"
+                onClick={handleClick}
+                active={active === "login"}
+              >
+              <Link to="/login">
+                <Button>Login</Button>
+              </Link>
+            </Menu.Item>
           )}
           {!isLoggedIn && (
-            <Link
-              to="/register"
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              <Button>Register</Button>
-            </Link>
+            <Menu.Item
+                name="register"
+                onClick={handleClick}
+                active={active === "register"}
+              >
+              <Link to="/register">
+                <Button primary>Register</Button>
+              </Link>
+            </Menu.Item>
           )}
         </Menu.Menu>
       </Menu>
