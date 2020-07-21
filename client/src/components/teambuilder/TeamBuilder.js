c82bb2293620967795790dd93b3d530e3b337493
Amokstakov <amoksyak@uoguelph.ca>
2020-07-21 00:19:13 -0400

    Added xs s md lg xl Responsive

--- client/src/components/teambuilder/TeamBuilder.js
+++ client/src/components/teambuilder/TeamBuilder.js
@@ -13,10 +13,12 @@ const TeamBuilder = () => {
   const Guard = useSelector((state) => state.TeamReducer.guard);
   const loadPlayers = useSelector((state) => state.Team.loadedPlayers);
 
+  console.log(loadPlayers);
+
   const RenderDisplay = (Zip) => {
     return Zip.map((zip, i) => {
       return (
-        <Col key={i} xs={24} md={6}>
+        <Col key={i} xs={24} sm={12} md={12} lg={12} xl={6}>
           <StatsDisplay Stats={zip[0]} Player={zip[1]} flag={flag} />
         </Col>
       );
@@ -50,7 +52,7 @@ const TeamBuilder = () => {
             {Forward.length > 0 && RenderDisplay(Forward)}
           </Row>
         </Col>
-        {loadPlayers.length > 0 && (
+        {loadPlayers[0].length > 0 && (
           <Col lg={{ offset: 2, span: 3 }} xs={20} sm={20} md={20}>
             <Team />
           </Col>
