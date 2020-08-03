422d08096e6a4f7155b638c22322aedd5ba55f4d
Amokstakov <amoksyak@uoguelph.ca>
2020-08-03 12:58:57 -0400

    Logout on Refresh

--- client/src/actions/AuthAction.js
+++ client/src/actions/AuthAction.js
@@ -15,7 +15,7 @@ import axios from "axios";
 import setAuthToken from "../utils/setAuthToken";
 import { FetchLastTenGames } from "./SingleSearchActions";
 
-const base_url = "https://enigmatic-reef-35438.herokuapp.com/";
+const base_url = "http://localhost:5001/";
 
 //Register User
 export const register = ({ first_name, last_name, email, password }) => async (
